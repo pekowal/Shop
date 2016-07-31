1941544fb6e62f990f97f0124cc78acca1562c84
pekowal <pekowal@gmail.com>
2016-07-31 13:29:27 +0200

    delete vendor

--- test/userTest.php
+++ test/userTest.php
@@ -4,6 +4,9 @@ require './../src/User.php';
 
 class userTest extends PHPUnit_Framework_TestCase
 {
-    public function testGetAllUsers()
+    public function testGetAllUsers(){
+        $user = new User();
+        $this->assertTrue();
+    }
 
 }
